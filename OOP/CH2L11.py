024a68c09d0161cd48e5ba88fdb45e92630805eb
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2025-12-16 07:01:44 -0500

    ...

--- OOP/CH2L11.py
+++ OOP/CH2L11.py
@@ -1,8 +1,8 @@
 class Dragon:
-    element = "ice"
+    # element = "ice"
 
     def __init__(self, element):
-        return
+        self.element = element
 
     def get_breath_damage(self):
         if self.element == "fire":
