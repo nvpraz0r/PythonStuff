4d610f94be862a61d6e50721153d6d18e63bec63
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2025-12-17 18:29:02 -0500

    chapter 3 lesson 8 complete

--- OOP/CH3L8.py
+++ OOP/CH3L8.py
@@ -1,18 +1,27 @@
 class BankAccount:
     def __init__(self, account_number, initial_balance):
-        pass
+        self.__account_number = account_number
+        self.__balance = initial_balance
 
     def get_account_number(self):
-        pass
+        return self.__account_number
 
     def get_balance(self):
-        pass
+        return self.__balance
 
     def deposit(self, amount):
-        pass
+        if amount <= 0:
+            raise ValueError("cannot deposit zero or negative funds")
+        else:
+            self.__balance += amount
 
     def withdraw(self, amount):
-        pass
+        if amount <= 0:
+            raise ValueError("cannot withdraw zero or negative funds")
+        elif amount > self.__balance:
+            raise ValueError("insufficient funds")
+        else:
+            self.__balance -= amount
 
 
 
