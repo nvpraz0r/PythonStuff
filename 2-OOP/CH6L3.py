bf036e7bdf62b6500ec198c1ad16a3e431b20ace
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2025-12-23 19:03:58 -0500

    I have several complaints about this lesson

--- 2-OOP/CH6L3.py
+++ 2-OOP/CH6L3.py
@@ -6,19 +6,16 @@
 
 class Rectangle:
     def overlaps(self, rect):
-        print("inc")
-        print(rect)
 
-        var = self.get_left_x()
-        print(rect.get_left_x())
-        print(rect.get_right_x())
-
-        print(var)
-
-        # x3 = self.get_left_x > rect.get_right_x
-
-        # if x3 == True:
-        #     print("asdf")
+        if(
+            self.get_left_x() <= rect.get_right_x() and
+            self.get_right_x() >= rect.get_left_x() and
+            self.get_top_y() >= rect.get_bottom_y() and
+            self.get_bottom_y() <= rect.get_top_y()
+        ):
+            return True
+        else:
+            return False
 
 
 
