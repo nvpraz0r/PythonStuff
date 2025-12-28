291d38e82544acc6d8f6af4b8ac8ad9838f07fb8
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2025-12-28 16:52:55 -0500

    int() conversion base was incorrect - I corrected it to reflect base 16 as opposed to the original value of 10

--- 3-Functional-Programming/CH1L14.py
+++ 3-Functional-Programming/CH1L14.py
@@ -1,9 +1,18 @@
 # 
 def hex_to_rgb(hex_color):
-    # ?
-    r = int(hex_color[:2], 10)
-    g = int(hex_color[2:4], 10)
-    b = int(hex_color[4:], 10)
+
+    # if len(str(hex_color)) != 6:
+    #     raise Exception("not a hex color string")
+
+    if is_hexadecimal(hex_color) == False:
+        raise Exception("not a hex color string")
+    elif len(str(hex_color)) != 6:
+        raise Exception("not a hex color string")
+
+
+    r = int(hex_color[:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:], 16)
     return r, g, b
 
 
