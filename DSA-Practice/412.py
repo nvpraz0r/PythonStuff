01f1c623fa8a5715e2ed3a5d028e471b4a2ab24b
Jonathan Parsons <jonathanaaronparsons@gmail.com>
2026-01-26 18:51:32 -0500

    no comment

--- DSA-Practice/412.py
+++ DSA-Practice/412.py
@@ -1,6 +1,32 @@
 def fizz_buzz(n: int) -> List[str]:
-    result = {}
+    
+    result = []
 
+    # 
+    for i in range(n):
+        # 
+        num = i + 1
 
+        # 
+        if num % 3 == 0 and num % 5 == 0:
+            result.append("FizzBuzz")
+        # 
+        elif num % 3 == 0:
+            result.append("Fizz")
+        # 
+        elif num % 5 == 0:
+            result.append("Buzz")
+        # 
+        else:
+            result.append(str(num))
 
+    # 
     return result
+
+
+def main():
+    fizz_buzz(5)
+
+
+if __name__ == '__main__':
+    main()
